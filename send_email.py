734bf296cb3e04d57c189543ba9525f6ba083385
Ankit Kumar Mohanty <ankitmohanty1804@gmail.com>
2021-08-30 13:00:29 +0530

    modifications done

--- send_email.py
+++ send_email.py
@@ -4,11 +4,11 @@ from email.mime.text import MIMEText
 def send_email(customer,dealer,rating,comments):
     port = 2525
     smtp_server = 'smtp.mailtrap.io'
-    login = '540916d7eaabe8'
-    password = '8c440437cf4e98'
+    login = 'Add login here'
+    password = 'Add password here'
     message = f'<h3>New Feedback Submission</h3><ul><li>Customer: {customer}</li><li>Dealer: {dealer}</li><li>Rating: {rating}</li><li>Comments: {comments}</li></ul>'
 
-    sender_mail = 'email1@example.com'
+    sender_mail = 'email1@example.com'    # Add original mails here
     receiver_mail = 'email2@example.com'
 
     msg = MIMEText(message, 'html')
